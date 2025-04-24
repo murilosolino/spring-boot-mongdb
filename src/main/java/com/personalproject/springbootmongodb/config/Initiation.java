6dd73ee98c66ca028f4b4674e5a6e440e1090322
murilosolino <murilo.solino@outlook.com>
2025-04-24 20:06:11 -0300

    config de instanciacao no mongodb

---
+++ src/main/java/com/personalproject/springbootmongodb/config/Initiation.java
@@ -0,0 +1,26 @@
+package com.personalproject.springbootmongodb.config;
+
+import com.personalproject.springbootmongodb.domain.User;
+import com.personalproject.springbootmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Initiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user1 = new User(null, "João Silva", "joao.silva@example.com");
+        User user2 = new User(null, "Maria Oliveira", "maria.oliveira@example.com");
+        User user3 = new User(null, "Carlos Santos", "carlos.santos@example.com");
+
+        userRepository.saveAll(Arrays.asList(user1,user2,user3));
+
+    }
+}
