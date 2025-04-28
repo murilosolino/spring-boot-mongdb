482ef0104f04a34b1c9a8c2fecfc25bcd34cf243
murilosolino <murilo.solino@outlook.com>
2025-04-28 18:04:28 -0300

    projecao dos dados do autor com dto

--- src/main/java/com/personalproject/springbootmongodb/config/Initiation.java
+++ src/main/java/com/personalproject/springbootmongodb/config/Initiation.java
@@ -2,6 +2,7 @@ package com.personalproject.springbootmongodb.config;
 
 import com.personalproject.springbootmongodb.domain.Post;
 import com.personalproject.springbootmongodb.domain.User;
+import com.personalproject.springbootmongodb.dto.AuthorDTO;
 import com.personalproject.springbootmongodb.repository.PostRepository;
 import com.personalproject.springbootmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,9 +33,9 @@ public class Initiation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(user1,user2,user3));
 
-        Post post1 = new Post(null, LocalDate.of(2023, 10, 1), "Título 1", "Conteúdo do post 1",user1);
-        Post post2 = new Post(null, LocalDate.of(2023, 10, 2), "Título 2", "Conteúdo do post 2",user2);
-        Post post3 = new Post(null, LocalDate.of(2023, 10, 3), "Título 3", "Conteúdo do post 3",user3);
+        Post post1 = new Post(null, LocalDate.of(2023, 10, 1), "Título 1", "Conteúdo do post 1", new AuthorDTO(user1));
+        Post post2 = new Post(null, LocalDate.of(2023, 10, 2), "Título 2", "Conteúdo do post 2",new AuthorDTO(user2));
+        Post post3 = new Post(null, LocalDate.of(2023, 10, 3), "Título 3", "Conteúdo do post 3",new AuthorDTO(user3));
 
         postRepository.saveAll(Arrays.asList(post1,post2,post3));
 
